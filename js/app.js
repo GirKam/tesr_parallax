0506a0bcc409a2ac2dd81727103ce4a5fa88215f
Kamil <e-kamil97-97@mail.ru>
2024-07-30 16:02:03 +0300

    add letter

--- js/app.js
+++ js/app.js
@@ -6,4 +6,13 @@ const swiper = new Swiper('.swiper', {
 	parallax:true
 	
 })
-console.log("object");
+
+document.querySelectorAll('.header-content h1').forEach(e => {
+	e.innerHTML = e.textContent.replace(/ (-|#|@){1}/g, s => s[1]+s[0]).replace(/(\S*)/g, m => {
+		return m.replace(/\S(-|#|@)?/g, '<span class="letter">$&</span>')
+	})
+	e.querySelectorAll('.letter').forEach(function(l, i) {
+		l.setAttribute('style', `z-index: -${ i }; transition-duration: ${ i/5 + 1 }s`)
+	})
+})
+
